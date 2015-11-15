ef1956cb5a5231f59212c6e395c82e3468509882
Zheng Fu <zf38@cornell.edu>
2015-11-14 20:45:57 -0500

    Initial Commit

---
+++ Hue Beats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hue Beats
+//
+//  Created by Zheng Fu on 11/14/15.
+//  Copyright © 2015 Zheng Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hue Beats/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hue Beats
+//
+//  Created by Zheng Fu on 11/14/15.
+//  Copyright © 2015 Zheng Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
